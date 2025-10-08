b2eb1cfe7d8a8ab46961a5dadbaa9d7135bda163
fayarsh <faydarsh2000@gmail.com>
2025-10-08 08:55:04 +0530

    feat: implement payment processing with Razorpay integration and authentication middleware

---
+++ Microservices/Payment/src/controllers/payment.controller.js
@@ -0,0 +1,95 @@
+const paymentModel = require("../models/payment.model");
+const axios = require("axios");
+
+require("dotenv").config();
+const Razorpay = require("razorpay");
+
+const razorpay = new Razorpay({
+  key_id: process.env.RAZORPAY_KEY_ID,
+  key_secret: process.env.RAZORPAY_KEY_SECRET,
+});
+
+async function createPayment(req, res) {
+  const token = req.cookies?.token || req.headers?.authorization?.split(" ")[1];
+
+  try {
+    const orderId = req.params.orderId;
+
+    const orderResponse = await axios.get(
+      `http://localhost:3003/api/orders/` + orderId,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+
+    const price = orderResponse.data.order.totalPrice;
+
+    const order = await razorpay.orders.create(price);
+
+    const payment = await paymentModel.create({
+      order: orderId,
+      razorpayOrderId: order.id,
+      user: req.user.id,
+      price: {
+        amount: order.amount,
+        currency: order.currency,
+      },
+    });
+
+    return res.status(201).json({
+      message: "Payment created successfully",
+      payment,
+    });
+  } catch (error) {
+    console.error("Error creating payment:", error);
+    return res.status(500).json({ error: "Internal server error" });
+  }
+}
+
+async function verifyPayment(req, res) {
+  const { razorpayOrderId, paymentId, signature } = req.body;
+
+  const secret = process.env.RAZORPAY_KEY_SECRET;
+
+  try {
+    const {
+      validatePaymentVerification,
+    } = require("../../node_modules/razorpay/dist/utils/razorpay-utils");
+
+    const isValid = validatePaymentVerification(
+      {
+        order_id: razorpayOrderId,
+        payment_id: paymentId,
+      },
+      signature,
+      secret
+    );
+
+    if (!isValid) {
+      return res.status(400).json({ message: "Invalid payment signature" });
+    }
+
+    const payment = await paymentModel.findOne({razorpayOrderId,status: "Pending",});
+
+    if (!payment) {
+      return res.status(404).json({ message: "Payment not found " });
+    }
+
+    payment.razorpayPaymentId = paymentId;
+    payment.status = "Completed";
+    payment.signature = signature;
+
+    
+    await payment.save();
+
+
+
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+}
+
+module.exports = { createPayment, verifyPayment };
---
+++ Microservices/Payment/src/routes/payment.routes.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const createAuthMiddleware = require("../../../Order/src/middlewares/auth.middleware");
+const {
+  createPayment,
+  verifyPayment,
+} = require("../controllers/payment.controller");
+
+const router = express.Router();
+
+router.post("/create/:orderId", createAuthMiddleware(["user"]), createPayment);
+
+router.post("/verify", createAuthMiddleware(["user"]), verifyPayment);
+
+module.exports = router;
